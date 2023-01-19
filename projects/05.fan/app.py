44ce3c5847ca4261f8d81e412cfc499b3a3ce5d5
Hye In Shin <hyein0608sa@gmail.com>
2023-01-19 12:58:31 +0900

    fan 2

--- projects/05.fan/app.py
+++ projects/05.fan/app.py
@@ -13,11 +13,17 @@ def home():
 def guestbook_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
-    return jsonify({'msg': 'POST 연결 완료!'})
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+    db.fan.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    all_comments = list(db.fan.find({},{'_id':False}))
+    return jsonify({'result': all_comments})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True)
